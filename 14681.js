dc8d3c9c23a03ef0018dfbb32ede16e43e0529a2
sminha <sminha8590@naver.com>
2024-09-28 01:34:51 +0900

    14681

---
+++ 14681.js
@@ -0,0 +1,13 @@
+let input = require('fs').readFileSync(0, "utf-8").toString().split('\n')
+
+const [x, y] = input.map(Number)
+
+let output
+
+if (x > 0) {
+    output = y > 0 ? '1' : '4'
+} else {
+    output = y > 0 ? '2' : '3'
+}
+
+console.log(output)
