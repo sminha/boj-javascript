f42abde437225a3ad5f306f43c55436d8f04b2b0
sminha <sminha8590@naver.com>
2024-09-29 23:41:57 +0900

    1. 입출력과 사칙연산

---
+++ 1. 입출력과 사칙연산/10869.js
@@ -0,0 +1,10 @@
+let input = require('fs').readFileSync('/dev/stdin').toString().split(' ')
+
+const a = Number(input[0])
+const b = Number(input[1])
+
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(Math.floor(a/b))
+console.log(a%b)
---
+++ 1. 입출력과 사칙연산/11382.js
@@ -0,0 +1,7 @@
+let input = require('fs').readFileSync('/dev/stdin').toString().split(' ')
+
+const a = Number(input[0])
+const b = Number(input[1])
+const c = Number(input[2])
+
+console.log(a+b+c)
