a98c750f8dd1222e7f1ccb92756025892e2ae717
sminha <sminha8590@naver.com>
2024-10-06 02:13:55 +0900

    15552

---
+++ 3. 반복문/15552.js
@@ -0,0 +1,12 @@
+let input = require('fs').readFileSync('/dev/stdin').toString().split('\n')
+
+const T = Number(input[0])
+
+let output = ''
+
+for(let i = 1; i <= T; i++) {
+  const [a, b] = input[i].split(' ').map(Number)
+  i !== T ? output += `${a+b}\n` : output +=`${a+b}`
+}
+
+console.log(output)
