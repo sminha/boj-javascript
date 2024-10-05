ba5cc56167e87e7d8211fa638742347d2fe8f68b
sminha <sminha8590@naver.com>
2024-10-06 02:14:42 +0900

    2439

---
+++ 3. 반복문/2439.js
@@ -0,0 +1,17 @@
+let input = require('fs').readFileSync('example.txt').toString()
+
+input = Number(input)
+
+let output = ''
+
+for (let i = 1; i <= input; i++) {
+  for (let j = 0; j < input - i; j++) {
+    output += ' '
+  }
+  for (let k = 0; k < i; k++) {
+    output += '*'
+  }
+  i !== input ? output += '\n' : output += ''
+}
+
+console.log(output)
