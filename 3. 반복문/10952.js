83b329da14a9070695ea13bfdae23c876ed38a45
sminha <sminha8590@naver.com>
2024-10-07 21:06:54 +0900

    10952

---
+++ 3. 반복문/10952.js
@@ -0,0 +1,25 @@
+let input = require('fs').readFileSync('example.txt').toString().split('\n')
+
+let output = ''
+
+// for (let i = 0; i < input.length; i++) {
+//   const [a, b] = input[i].split(' ').map(Number)
+
+//   if (a === 0 && b === 0) break
+
+//   output += `${a+b}\n`
+// }
+
+// console.log(output.trim())
+
+let i = 0
+
+while (input[i] !== '0 0') {
+  const [a, b] = input[i].split(' ').map(Number)
+
+  output += `${a+b}\n`
+
+  i += 1
+}
+
+console.log(output.trim())
