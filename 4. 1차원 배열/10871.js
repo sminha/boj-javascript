6f4dfb6332aafe1b7060bc4555720eff24396d70
sminha <sminha8590@naver.com>
2024-10-07 21:59:27 +0900

    10871

---
+++ 4. 1차원 배열/10871.js
@@ -0,0 +1,19 @@
+let input = require('fs').readFileSync('example.txt').toString().split('\n')
+
+const N = Number(input[0].split(' ')[0])
+const X = Number(input[0].split(' ')[1])
+
+let output = ''
+
+// for (let i = 0; i < N; i++) {
+//   let number = input[1].split(' ')[i]
+//   if (number < X) output += `${number} `
+// }
+
+// console.log(output.trim())
+
+input[1].split(' ')
+  .filter((value) => value < X)
+  .forEach((value) => output += `${value} `)
+
+console.log(output.trim())
