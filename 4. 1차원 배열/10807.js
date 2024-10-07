cc235bc9095360c40a94e79b2f38de6fbae1716e
sminha <sminha8590@naver.com>
2024-10-07 21:59:15 +0900

    10807

---
+++ 4. 1차원 배열/10807.js
@@ -0,0 +1,17 @@
+let [N, input, v] = require('fs').readFileSync('example.txt').toString().split('\n')
+
+N = Number(N)
+v = Number(v)
+
+input = input.split(' ').map(Number)
+
+let output = 0
+
+// for (let i = 0; i < N; i++) {
+//   if (input[i] !== v) continue
+//   else output += 1
+// }
+
+output = input.filter((value) => value === v).length
+
+console.log(output)
