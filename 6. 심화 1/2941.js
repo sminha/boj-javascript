f8d93407b09442b663dbb5485156959f3655739f
sminha <sminha8590@naver.com>
2025-02-14 19:39:13 +0900

    fix: Remove Debugging console.log(word)

--- 6. 심화 1/2941.js
+++ 6. 심화 1/2941.js
@@ -1,7 +1,5 @@
 let word = require('fs').readFileSync('/dev/stdin').toString().trim()
 
-console.log(word)
-
 const croatia = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
 
 for (const c of croatia) {
