20ef66e92df90b3540d8c000f8035438dfa61037
sminha <sminha8590@naver.com>
2025-08-02 16:36:59 +0900

    19532

--- 12. 브루트 포스/19532.js
+++ 12. 브루트 포스/19532.js
@@ -3,6 +3,7 @@ let [a, b, c, d, e, f] = require('fs').readFileSync('/dev/stdin').toString().spl
 for (let x = -999; x <= 999; x++) {
   for (let y = -999; y <= 999; y++) {
     if (a * x + b * y === c && d * x + e * y === f) {
+      // 첨에 (a-d) * x + (b-e) * y === c - f 이거로 해서 틀림
       console.log(x, y)
       return
     }
